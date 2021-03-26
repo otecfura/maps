c70b4e72107bf82d098ecdcd7a00512db8a1d050
otecfura <fura.otec@gmail.com>
2021-03-26 20:44:49 +0100

    feat(init): initial commit

---
+++ src/App.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import L from "leaflet";
+  import { onMount } from "svelte";
+  import Setup from "./components/Setup.svelte";
+
+  export let name: string;
+  let map;
+  let lat;
+  var layerGroup;
+
+  onMount(async () => {
+    map = L.map("map").fitWorld();
+    layerGroup = L.layerGroup().addTo(map);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+    }).addTo(map);
+    map.locate({ setView: true, maxZoom: 16 });
+    map.on("locationfound", onLocationFound);
+    map.on("locationerror", onLocationError);
+  });
+
+  function onLocationFound(e) {
+    var radius = e.accuracy;
+    lat = e;
+
+    L.circle(e.latlng, radius, { color: "red", opacity: 0.5 }).addTo(map);
+  }
+
+  function onLocationError(e) {
+    alert(e.message);
+  }
+
+  function handleMessage(event) {
+    if (event.detail.text) {
+      L.circle(lat.latlng, event.detail.text).addTo(layerGroup);
+    } else {
+      layerGroup.clearLayers();
+    }
+  }
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""
+  />
+</svelte:head>
+
+<div class="container">
+  <div id="map" class="box" />
+  <Setup on:changeSize={handleMessage} />
+</div>
+
+<style>
+  .container {
+    height: 100%;
+    width: 100vw;
+  }
+
+  .box {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0.7;
+    background: #0057e3;
+  }
+</style>
---
+++ src/components/Setup.svelte
@@ -0,0 +1,102 @@
+<script type="ts">
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  let size = 128;
+  let zmenseni;
+
+  let SlunceMerkur = 0;
+  let SlunceVenuse = 0;
+  let SlunceZeme = 0;
+  let ZemeMesic = 0;
+  let SlunceMars = 0;
+  let SlunceJupiter = 0;
+  let SlunceSaturn = 0;
+  let SlunceUran = 0;
+  let SlunceNeptun = 0;
+  let SluncePluto = 0;
+  let SlunceEris = 0;
+
+  let arr = [
+    ["Slunce -> Merkur", SlunceMerkur],
+    ["Slunce -> Venuse", SlunceVenuse],
+    ["Slunce -> Zeme", SlunceZeme],
+    ["Zeme -> Mesic", ZemeMesic],
+    ["Slunce -> Mars", SlunceMars],
+    ["Slunce -> Jupiter", SlunceJupiter],
+    ["Slunce -> Saturn", SlunceSaturn],
+    ["Slunce -> Uran", SlunceUran],
+    ["Slunce -> Neptun", SlunceNeptun],
+    ["Slunce -> Pluto", SluncePluto],
+    ["Slunce -> Eris", SlunceEris],
+  ];
+
+  onSizeChange();
+
+  function onDataChange(val) {
+    dispatch("changeSize", {
+      text: val,
+    });
+  }
+
+  function onSizeChange() {
+    zmenseni = 1392700000 / (size / 100);
+    SlunceMerkur = 57909050000 / zmenseni;
+    SlunceVenuse = 108208000000 / zmenseni;
+    SlunceZeme = 149598023000 / zmenseni;
+    ZemeMesic = 38439900000 / zmenseni;
+    SlunceMars = 227939200000 / zmenseni;
+    SlunceJupiter = 5.2044 * size;
+    SlunceSaturn = 9.5826 * size;
+    SlunceUran = 19.2184 * size;
+    SlunceNeptun = 30.07 * size;
+    SluncePluto = 49.305 * size;
+    SlunceEris = 67.864 * size;
+    arr = [
+      ["Slunce -> Merkur", SlunceMerkur],
+      ["Slunce -> Venuse", SlunceVenuse],
+      ["Slunce -> Zeme", SlunceZeme],
+      ["Zeme -> Mesic", ZemeMesic],
+      ["Slunce -> Mars", SlunceMars],
+      ["Slunce -> Jupiter", SlunceJupiter],
+      ["Slunce -> Saturn", SlunceSaturn],
+      ["Slunce -> Uran", SlunceUran],
+      ["Slunce -> Neptun", SlunceNeptun],
+      ["Slunce -> Pluto", SluncePluto],
+      ["Slunce -> Eris", SlunceEris],
+    ];
+  }
+</script>
+
+<div class="overlay">
+  <p>Výška</p>
+  <input type="text" size="10" bind:value={size} on:change={onSizeChange} />
+  {#each arr as cat}
+    <p class="size" on:click={() => onDataChange(cat[1])}>
+      {cat[0]} : {cat[1].toFixed(2)} (m)
+    </p>
+  {/each}
+  <button on:click={() => onDataChange(null)}>clear</button>
+</div>
+
+<style>
+  .size:hover {
+    background-color: aqua;
+  }
+  .overlay {
+    width: 250px;
+
+    position: fixed;
+    right: 0;
+    z-index: 50;
+    margin-right: 50px;
+    padding: 10px;
+    background: #ffffff;
+  }
+  p {
+    font-size: medium;
+    cursor: pointer;
+    padding: 5px;
+  }
+</style>
